391631c103c058741f463ab27611ed3a3322272e
ParmarMohitDeveloper <mohitparmardeveloper@gmail.com>
2025-11-04 11:49:40 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/auth/",
+  baseURL: "https://goggle-login-mern-webapp-backend.onrender.com/",
   withCredentials: true,
 });
 
